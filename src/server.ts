45a4463dec86e0de3382126aec0b7b150b4b12b7
kostiantynn <kostya.code@gmail.com>
2021-08-02 21:15:33 +0300

    cors enable

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import cookie from "fastify-cookie";
+import cors from "fastify-cors";
 import * as gigabyte from "./modules/brandshop-gigabyte/routers";
 import * as admin from "./modules/admin/routers";
 import { Server } from "./Server/Server";
@@ -7,6 +8,7 @@ import { Server } from "./Server/Server";
 const server = new Server(fastify({ logger: true, bodyLimit: 10485760 }));
 
 server.registerPlugin({ pluginInstance: cookie, options: { secret: "hehe" } });
+server.registerPlugin({ pluginInstance: cors, options: { origin: true } });
 server.registerRouter(gigabyte);
 // server.registerRouter(admin);
 
