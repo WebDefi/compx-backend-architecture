f17039a3a65e34da2fc855dad0198e7765e778e9
kostiantynn <kostya.code@gmail.com>
2021-10-16 19:02:10 +0300

    admin upload data

--- src/modules/admin/routers/controller.ts
+++ src/modules/admin/routers/controller.ts
@@ -7,27 +7,32 @@ export const uploadXmlData = async (
   req: FastifyRequest<RouteGenericInterfacePostXml>,
   rep: FastifyReply
 ) => {
-  const xmlFile = Buffer.from(req.body.xmlData, "base64");
-  const xmlData = await Brandshop.parseXml(xmlFile, {
-    category: { id: true, name: true },
-    item: {
-      categoryId: true,
-      name: true,
-      description: true,
-      url: true,
-      images: true,
-      priceRUAH: true,
-      detailedDescriptionRU: true,
-      detailedDescriptionUA: true,
-      stock: true,
-      characteristics: true,
-      condition: true
-    },
-  });
-  // xmlData.items.forEach((item: any) => console.log(item.characteristics.charItem));
-  const insertData = await adminService.createRecord(xmlData);
-  if (insertData.error) {
-    return rep.status(400).send(insertData);
+  console.log(req.body);
+  try {
+    const xmlFile = Buffer.from(req.body.xmlData, "base64");
+    const xmlData = await Brandshop.parseXml(xmlFile, {
+      category: { id: true, name: true },
+      item: {
+        categoryId: true,
+        name: true,
+        description: true,
+        url: true,
+        images: true,
+        priceRUAH: true,
+        detailedDescriptionRU: true,
+        detailedDescriptionUA: true,
+        stock: true,
+        characteristics: true,
+        condition: true,
+      },
+    });
+    // xmlData.items.forEach((item: any) => console.log(item.characteristics.charItem));
+    const insertData = await adminService.createRecord(xmlData);
+    if (insertData.error) {
+      return rep.status(400).send(insertData);
+    }
+    return rep.status(201).send(insertData);
+  } catch (error) {
+    return rep.status(400).send({ error });
   }
-  return rep.status(201).send(insertData);
 };
